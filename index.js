04824a3dbce8470a3f76f4ac33cd0340850aa5c2
Nobu <rockmusic2090@gmail.com>
2020-02-12 22:00:03 -0800

    Prepare for deploy to heroku

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ app.use(post);
 
 const PORT = process.env.PORT || 5000;
 
+if (process.env.NODE_ENV === "production") {
+  const path = require("path");
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(PORT, () => {
   console.log(`Suerver is running on port ${PORT}`);
 });
